41834664a9129998f08bd78c8d307d7549a18a1f
Arun P Muthu <apm0366@gmail.com>
2023-01-04 22:40:05 +0800

    update hosts

--- savings_visualiser/settings.py
+++ savings_visualiser/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k5+89liqjozvb#+jz)y0ha@en@yntq*r!b_*h!c73ux=__z)7g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['savings-visualiser-env.eba-daupks2m.ap-southeast-2.elasticbeanstalk.com']
 
 # Application definition
 
