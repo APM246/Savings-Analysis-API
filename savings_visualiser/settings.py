f16fad16cea64fee7709074c3db72167f21f2e56
Arun P Muthu <apm0366@gmail.com>
2023-01-23 00:01:46 +0800

    add forward slash

--- savings_visualiser/settings.py
+++ savings_visualiser/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200', 'https://savings-visualiser-m03d04crb-apm246.vercel.app'
+    'http://localhost:4200', 'https://savings-visualiser.vercel.app/'
 ]
 ROOT_URLCONF = 'savings_visualiser.urls'
 
